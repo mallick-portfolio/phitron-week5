6752bfc88492d58cd46c0f7843c522ab7da0cce7
mallick-portfolio <tamal1714019@gmail.com>
2023-12-12 01:39:44 +0600

    filter post by category

--- p_day1/app/urls.py
+++ p_day1/app/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("profile/", views.profile, name="profile"),
     path("post-details/<int:id>/", views.post_details, name="post_details"),
+    path("post-category/<slug:category_slug>/", views.home, name="category_post"),
 ]
--- p_day1/app/views.py
+++ p_day1/app/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from posts.models import Post
+from posts.models import Post, Category
 # Create your views here.
-def home(request):
-  posts = Post.objects.all()
-  return render(request, './home.html', {"posts": posts})
+def home(request, category_slug = None):
+  if category_slug:
+    posts = Post.objects.filter(category__slug =category_slug)
+  else:
+    posts = Post.objects.all()
+  categories = Category.objects.all()
+  return render(request, './home.html', {"posts": posts ,"categories": categories})
 
 @login_required
 def profile(request):

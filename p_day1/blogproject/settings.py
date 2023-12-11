9ae4b2050ee02027b65fde746213802c4d270b0e
mallick-portfolio <tamal1714019@gmail.com>
2023-12-12 00:46:15 +0600

    added blog post

--- p_day1/blogproject/settings.py
+++ p_day1/blogproject/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'app',
     'crispy_forms',
     'crispy_bootstrap5',
+    'posts'
 
 ]
 
---
+++ p_day1/posts/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from posts.models import Category, Post
+# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+  prepopulated_fields = {"slug": ("name",)}
+  # readonly_fields = ['slug']
+
+
+
+admin.site.register(Post)
+admin.site.register(Category, CategoryAdmin)

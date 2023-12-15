1cd12599ee082bdfda1fd0d27a0fdff454da7a17
mallick-portfolio <tamal1714019@gmail.com>
2023-12-15 23:03:37 +0600

    added assignment project

---
+++ assignment/app/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from car.models import Car, Brand
+# Create your views here.
+def home(request, brand_name = None):
+  brands = Brand.objects.all()
+  if brand_name:
+    cars = Car.objects.filter(brand__name = brand_name)
+  else:
+    cars = Car.objects.all()
+  return render(request, './home.html', {"cars": cars, "brands": brands})
+
+@login_required
+def profile(request):
+  
+  return render(request, './profile.html',)
+
---
+++ assignment/car/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+class Brand(models.Model):
+  name = models.CharField(max_length=20)
+
+
+class Car(models.Model):
+  title = models.CharField(max_length=200)
+  description = models.TextField()
+  price = models.FloatField()
+  image = models.ImageField(upload_to='images/')
+  brand = models.ForeignKey(Brand, on_delete=models.CASCADE, null=True, blank=True, related_name="brands")
+  quantity = models.IntegerField(blank=True, null=True)
+
+  def __str__(self) -> str:
+    return self.title
+  
+class Order(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE,related_name="users")
+  car = models.ForeignKey(Car, on_delete=models.CASCADE, related_name="cars")
+  
